a8ea5bb7cd5b0b9d3128c1060e92e09e2a672eb6
Xhaitornay <134662492+Xhaitornay@users.noreply.github.com>
2023-07-25 19:21:09 -0600

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -1,24 +1,32 @@
 class Game {
-  constructor() {}
-
+  constructor() {
+    //this.resetTitle=createElement("h2");
+    //this.resetButton=createButton("");
+  }
+  //BP
   getState() {
     var gameStateRef = database.ref("gameState");
     gameStateRef.on("value", function(data) {
       gameState = data.val();
     });
   }
+  //BP
   update(state) {
     database.ref("/").update({
       gameState: state
     });
   }
 
+  // AM
   start() {
     player = new Player();
     playerCount = player.getCount();
 
     form = new Form();
     form.display();
+
+    fuels= new Group();
+    powerCoins= new Group();
 
     car1 = createSprite(width / 2 - 50, height - 100);
     car1.addImage("car1", car1_img);
@@ -30,61 +38,44 @@ class Game {
 
     cars = [car1, car2];
 
-    // C38 AM
-    fuels = new Group();
-    powerCoins = new Group();
-
-    // Agregando sprites de combustible en el juego
-    this.addSprites(fuels, 4, fuelImage, 0.02);
-
-    // Agregando sprites de monedas en el juego
-    this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
+    this.addSprites(fuels, 10, fuelImage, 0.02)
+    this.addSprites(powerCoins, 20, powerCoinImage, 0.09)
   }
 
-  // C38 AM
-  addSprites(spriteGroup, numberOfSprites, spriteImage, scale) {
-    for (var i = 0; i < numberOfSprites; i++) {
-      var x, y;
-
-      x = random(width / 2 + 150, width / 2 - 150);
-      y = random(-height * 4.5, height - 400);
-
-      var sprite = createSprite(x, y);
-      sprite.addImage("sprite", spriteImage);
-
-      sprite.scale = scale;
-      spriteGroup.add(sprite);
-    }
-  }
-
+  //BP
   handleElements() {
     form.hide();
     form.titleImg.position(40, 50);
     form.titleImg.class("gameTitleAfterEffect");
+
+   // this.resetTitle.html("REINICIAR JUEGO");
+    //this.resetTitle.class("resetText");
+    //this.resetTitle.position(width/2+200, 40);
+    //this.resetButton.position(width/2+230, 100);
   }
 
+  // AA
   play() {
     this.handleElements();
 
-    Player.getPlayersInfo();
-
+    Player.getPlayersInfo(); // Agregado
+this.handleResetButton
     if (allPlayers !== undefined) {
       image(track, 0, -height * 5, width, height * 6);
 
       // indice del arreglo
       var index = 0;
       for (var plr in allPlayers) {
-        // Agrega 1 al índice en cada ciclo
-        index = index + 1;
-
         // Usa datos de la base de datos para mostrar los autos en dirección x e y
         var x = allPlayers[plr].positionX;
         var y = height - allPlayers[plr].positionY;
 
-        cars[index - 1].position.x = x;
-        cars[index - 1].position.y = y;
+        cars[index-1].position.x = x;
+        cars[index-1].position.y = y;
 
-        // C38  AA
+        // Agrega 1 al índice en cada ciclo
+        index = index + 1;
+
         if (index === player.index) {
           stroke(10);
           fill("red");
@@ -93,6 +84,8 @@ class Game {
           this.handleFuel(index);
           this.handlePowerCoins(index);
 
+          camera.position.x = cars[index-1].position.x
+          camera.position.y = cars[index-1].position.y;
         }
       }
 
@@ -101,29 +94,22 @@ class Game {
         player.positionY += 10;
         player.update();
       }
-
+ 
       drawSprites();
     }
   }
 
-  handleFuel(index) {
-    // Agregando combustible
-    cars[index - 1].overlap(fuels, function(collector, collected) {
-      player.fuel = 185;
-      // "collected" es el sprite en el grupo de coleccionables que detona
-      // el evento
-      collected.remove();
-    });
-  }
+addSprites(SpriteGroup, numberSprites, spriteImage, scale){
+  for(var i=0; i < numberSprites; i++){
+    var x,y;
+    x=random ( width/2+150);
+    y=random(-height+4.5, height-400);
 
-  handlePowerCoins(index) {
-    // Agregando monedas
-    cars[index - 1].overlap(powerCoins, function(collector, collected) {
-      player.score += 21;
-      player.update();
-      //"collected" es el sprite en el grupo de coleccionables que detona
-      // el evento
-      collected.remove();
-    });
+    var sprite = createSprite(x,y);
+    sprite.addImage("Sprite", spriteImage);
+    sprite.scale=scale;
+    SpriteGroup.add(sprite);
   }
+}
+
 }
